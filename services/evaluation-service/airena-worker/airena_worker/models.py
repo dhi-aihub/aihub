bee2f725d45eb818e3307e20c57d8e8d709299a9
Yifeng Li <charlesliyifeng@gmail.com>
2025-08-12 16:08:36 +0800

    Add raw_log

--- services/evaluation-service/airena-worker/airena_worker/models.py
+++ services/evaluation-service/airena-worker/airena_worker/models.py
@@ -19,8 +19,9 @@ class QueueInfo:
 
 
 class ExecutionOutput:
-    def __init__(self, ok: bool, result: Union[str, None], error=Union[str, None]):
+    def __init__(self, ok: bool, raw_log: str, result: Union[str, None], error: Union[str, None]):
         self.ok = ok
+        self.raw_log = raw_log
         self.result = result
         self.error = error
         
